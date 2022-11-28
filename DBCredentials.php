b81408e7a886b6f3bf7dee915bd00d6898e99d82
Aidam <55688345+Aidam7@users.noreply.github.com>
2022-11-28 10:01:36 +0100

    Update DBCredentials.php

--- DBCredentials.php
+++ DBCredentials.php
@@ -1,7 +1,7 @@
 <?php
-DEFINE("DBUsername","www-aplikace");
-DEFINE("DBPassword","Bezpe4n0Heslo.");
-DEFINE("DB","ip_3");
-DEFINE("DBHost","127.0.0.1");
-DEFINE("DBCharset","utf8mb4");
+DEFINE("DBUsername","");
+DEFINE("DBPassword","");
+DEFINE("DB","");
+DEFINE("DBHost","");
+DEFINE("DBCharset","");
 ?>
